1c4b42e6d83c11f4e45a2f3d8f35aa7cf5db150e
dassouvik2004 <souvikdas9102004@gmail.com>
2025-04-20 03:06:18 +0530

    Datatype

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,1 +1,18 @@
-console.log("Souvik Das, He is a good boy. I am try to learn JS. It is tough but I can do!");
+let name = "Souvik Das"
+let age = 18
+let loggedIn = true
+let temparature = null
+
+console.log(typeof "name") // string
+console.log(typeof age) // nuumber
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
+// These are the some datatypes of JS:
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// symbol
+// undefined
